7978808800ce1209880738cbacbe02d3dda8e7ca
Dexter Corley <dexter.corley@live.com>
2024-04-04 08:55:06 -0700

    Removed image prop

--- src/app/landingpage/descriptions.tsx
+++ src/app/landingpage/descriptions.tsx
@@ -5,7 +5,6 @@ import Link from "next/link";
 const Descriptions = () => {
   return (
     <div className="my-40 absolute right-6 inline-flex h-80 w-96 flex-col items-start justify-center gap-6">
-        <Image></Image>
       <div className="self-stretch font-['Inter'] text-4xl font-semibold leading-10 text-black">
         AI Chat that knows your vessel
       </div>
