f22a8e5148ce415e2f0ce204b52ced432c98f45d
Myles <myleshansen@outlook.com>
2024-05-30 19:30:16 -0700

    Setup pdf db table + increase max pdf file upload size

--- nord-app/src/app/api/uploadthing/core.ts
+++ nord-app/src/app/api/uploadthing/core.ts
@@ -8,7 +8,7 @@ const f = createUploadthing();
 // FileRouter for your app, can contain multiple FileRoutes
 export const ourFileRouter = {
     // Define as many FileRoutes as you like, each with a unique routeSlug
-    pdfUploader: f({ pdf: { maxFileSize: "4MB" } })
+    pdfUploader: f({ pdf: { maxFileSize: "32MB" } })
         // Set permissions and file types for this FileRoute
         .middleware(async ({ req }) => {
             // This code runs on your server before upload
