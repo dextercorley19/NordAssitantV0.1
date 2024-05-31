ff8609d6e37b2650e813e1710c888a2e9c813f8f
Myles <myleshansen@outlook.com>
2024-05-30 17:05:00 -0700

    Add functionality to donwload file from pdf-parser server

--- pdf-server/main.py
+++ pdf-server/main.py
@@ -2,6 +2,11 @@ from contextlib import asynccontextmanager
 import uvicorn
 from fastapi import FastAPI
 from db import test_pool, pool
+import requests
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 
 @asynccontextmanager
@@ -20,6 +25,26 @@ app = FastAPI(lifespan=lifespan)
 async def test_db():
     results = await test_pool()
     return results
+
+
+@app.get("/files")
+async def get_files():
+    url = os.getenv("PDF_URL")
+    response = requests.get(url, stream=True)
+
+    # isolate PDF filename from URL
+    pdf_file_name = os.path.basename(url)
+    if response.status_code == 200:
+        # Save in current working directory
+        filepath = os.path.join(os.getcwd(), pdf_file_name)
+        with open(filepath, "wb") as pdf_object:
+            pdf_object.write(response.content)
+            print(f"{pdf_file_name} was successfully saved!")
+    else:
+        print(f"Uh oh! Could not download {pdf_file_name},")
+        print(f"HTTP response status code: {response.status_code}")
+
+    return {"message": "File downloaded successfully!"}
 
 
 if __name__ == "__main__":
